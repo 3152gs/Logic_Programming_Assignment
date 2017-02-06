831ccad1ff98ecabebc1a7541afaee9f4f245432
Gauri Shankar Gautam <gaurishankargautam@Gauris-MacBook-Air.local>
2017-02-05 20:56:44 -0500

    Second case added to take care of non-empty list. Not working

--- lp.pl
+++ lp.pl
@@ -2,5 +2,6 @@
 % Function to find the sum of the numbers in a simple list.
 
 sum-up-numbers-simple([],0).   %sum of the numbers in empty list is 0.
-
+sum-up-numbers-simple(X|Y,N) :- sum-up-numbers-simple(Y, N1),              %recursive case for a non-empty list.
+								N is X + N1. 
 
