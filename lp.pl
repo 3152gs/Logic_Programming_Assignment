fdc17c4782f2c5c6fb90921e63c539f6b79ca659
Gauri Shankar Gautam <gaurishankargautam@Gauris-MacBook-Air.local>
2017-02-05 21:23:20 -0500

    Fixed the old bug to get it work correctly for a simple list of numbers.

--- lp.pl
+++ lp.pl
@@ -2,6 +2,7 @@
 % Function to find the sum of the numbers in a simple list.
 
 sum-up-numbers-simple([],0).   %sum of the numbers in empty list is 0.
-sum-up-numbers-simple(X|Y,N) :- sum-up-numbers-simple(Y, N1),              %recursive case for a non-empty list.
+sum-up-numbers-simple([X|Y],N) :-  %recursive case for a non-empty list.
+								sum-up-numbers-simple(Y, N1),              
 								N is X + N1. 
 
