c6febfb2d14c86babb9c6beeafa09be9b854114b
Gauri Shankar Gautam <gaurishankargautam@Gauris-MacBook-Air.local>
2017-02-05 22:10:21 -0500

    Passed all the test cases. Final solution for Q1.

--- lp.pl
+++ lp.pl
@@ -3,13 +3,12 @@
 
 sum-up-numbers-simple([],0).   %sum of the numbers in empty list is 0.
 
-sum-up-numbers-simple([X|Y],N) :-	
-									\+(number(X)),
+sum-up-numbers-simple([X|Y],N) :-	%checks if the item is anumber, if not a number simply add 0 to the accumulator.
+									\+(number(X)),  
 									sum-up-numbers-simple(Y, N1),
 									N is 0 + N1. 
 sum-up-numbers-simple([X|Y],N) :-  %recursive case for a non-empty list.
-								(is_list(X)), 
-								(number(X)),
+								\+(is_list(X)),  %check if the item is list or not
+								number(X),       %check if the item is a number
 								sum-up-numbers-simple(Y, N1),              
 								N is X + N1. 
-
